7045ff8dc03f93b8b43388bcafd030a8a4931847
陳友宣 <chenyoxuan@chenyouxuandeMacBook-Air.local>
2022-06-17 14:24:51 +0800

    vue-config fixed

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/weatherapp/' // test20200915 為 repo 名稱
+    ? '/weatherapp/dist/' // test20200915 為 repo 名稱
     : '/'
 }
